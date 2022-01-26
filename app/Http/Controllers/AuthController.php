47f370a569514bfba8d4832cac2b418c8e466c83
Ali Afthab <aliallu3xa@gmail.com>
2022-01-26 21:41:03 +0530

    implemented logout and login features with the help of sanctum tokens and tested successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Hashing\BcryptHasher;
-use CreatePersonalAccessTokensTable;
-use GuzzleHttp\Promise\Create;
+
+//use Illuminate\Hashing\BcryptHasher;
+//use CreatePersonalAccessTokensTable;
+//use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+//use Illuminate\Validation\Rules\Exists;
+//use Laravel\Sanctum\PersonalAccessToken
+
 //use Laravel\Sanctum\HasApiTokens ;
 
 class AuthController extends Controller
@@ -23,7 +28,7 @@ class AuthController extends Controller
             'email' => $field['email'],
             'password' => bcrypt($field['password'])
         ]);
-        $token = $users->createToken('userToken')->plainTextToken;
+         $token = $users->createToken('userToken')->plainTextToken;
 
         $response = [
             'users'=>$users,
@@ -38,25 +43,44 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $field = $request->validate([
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|alpha_num|min:8|confirmed'
+            'email' => 'required|email',
+            'password' => 'required|alpha_num|min:8',
         ]);
 
-        //EMAIL VERIFICATION
-        $users = User::where('email');
-        $token = $users->createToken('userToken')->plainTextToken;
 
-        $response = [
-            'users'=>$users,
-            'token'=>$token
-        ];
 
-        return response($response);
-    }
+            //EMAIL VERIFICATION
+            $users = User::where('email',$field['email'])->first();
+
+            //PASSWORD VERIFICATION
+            if(!$users || !Hash::check($field['password'], $users->password)){
+                return response([
+                    'message'=>'No Email or password ! please try again'
+                ]);
+
+            }
+
+
+            $token = $users->createToken('userToken')->plainTextToken;
+
+            $response = [
+                'users'=>$users,
+                'token'=>$token
+            ];
+
+            return response($response);
+          }
+
+
+
 
     public function logout(Request $request){
         auth()->user()->tokens()->delete();
 
         return response(['message'=>'Logged out']);
+    }
+
+    public function Users(){
+        return User::all();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::post('/login', [AuthController::class,'login']);
 Route::get('/products/search/{name}',[ProductController::class,'search']);
 Route::get('/products',[ProductController::class,'index']);
 Route::get('/products/{id}',[ProductController::class,'show']);
-
+Route::get('/users',[AuthController::class,'Users']);
 
 
 Route::group(['middleware'=>'auth:sanctum'],function () {
