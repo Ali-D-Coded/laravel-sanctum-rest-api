c4ac31bed460bca4fefe85d0da292d6942b079b0
Ali Afthab <aliallu3xa@gmail.com>
2022-01-26 19:25:31 +0530

    created logout route, controller and tested successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Hashing\BcryptHasher;
 use CreatePersonalAccessTokensTable;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
+//use Laravel\Sanctum\HasApiTokens ;
 
 class AuthController extends Controller
 {
@@ -30,5 +31,32 @@ class AuthController extends Controller
         ];
 
         return response($response);
+    }
+
+    //LOGIN
+
+    public function login(Request $request)
+    {
+        $field = $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|alpha_num|min:8|confirmed'
+        ]);
+
+        //EMAIL VERIFICATION
+        $users = User::where('email');
+        $token = $users->createToken('userToken')->plainTextToken;
+
+        $response = [
+            'users'=>$users,
+            'token'=>$token
+        ];
+
+        return response($response);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return response(['message'=>'Logged out']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //Route::resource('/products', ProductController::class);
-Route::post('register', [AuthController::class,'register']);
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
 Route::get('/products/search/{name}',[ProductController::class,'search']);
 Route::get('/products',[ProductController::class,'index']);
 Route::get('/products/{id}',[ProductController::class,'show']);
@@ -29,6 +30,7 @@ Route::group(['middleware'=>'auth:sanctum'],function () {
     Route::post('/products',[ProductController::class,'store']);
     Route::put('/products/{id}',[ProductController::class,'update']);
     Route::delete('/products/{id}',[ProductController::class,'destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
